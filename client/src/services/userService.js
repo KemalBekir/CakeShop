b5f67bc2fbb462cf6273100317d4580b34087b50
KemalBekir <95451696+KemalBekir@users.noreply.github.com>
2022-10-20 09:27:09 +0100

    authContext, custom hook and user service

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -17,3 +17,23 @@ export async function registerUser({ username, email, password }) {
         body: JSON.stringify({ username, email, password, tel }),
       }).then((data) => data.json());
 }
+
+export async function getProfile(token) {
+    return fetch("http://localhost:5000/users/profile", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "X-Authorization": token,
+        },
+    }).then((data) => data.json());
+}
+
+export async function logout(token){
+    return fetch('http://localhost:5000/users/logout', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Authorization': token
+        }
+    }).then(() => localStorage.clear());
+}
