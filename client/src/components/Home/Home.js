0286d687ba2b96d4c8d1a03c2071a1cf954efd73
KemalBekir <95451696+KemalBekir@users.noreply.github.com>
2022-10-22 18:38:47 +0100

    home component  initial html

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -3,7 +3,13 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="home" >Home</div>
+    <section className='home-container'>
+      <article className="home-section">
+        <div className="home-img-container">
+          <img className="home-img" src='https://spanishsabores.com/wp-content/uploads/2020/05/DSC08145.jpg' alt='cake covered on top with chocolate'></img>
+        </div>
+      </article>
+    </section>
   )
 }
 
