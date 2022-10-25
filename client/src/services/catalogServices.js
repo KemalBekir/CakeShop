1ce7d1febfeeeee248361e3edb612e386d7648fd
KemalBekir <95451696+KemalBekir@users.noreply.github.com>
2022-10-25 15:56:42 +0100

    added more endpoints to catalogServices

--- client/src/services/catalogServices.js
+++ client/src/services/catalogServices.js
@@ -1,10 +1,79 @@
 const catalogUrl = "http://localhost:5000/catalog";
 
 export async function getAll() {
-    return fetch(`${catalogUrl}/`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    }).then((data) => data.json());
+  return fetch(`${catalogUrl}/`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((data) => data.json());
+}
+
+export async function getTopFive() {
+  return fetch(`${catalogUrl}/top5`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((data) => data.json());
+}
+
+export async function search(text) {
+  return fetch(`${catalogUrl}/search?text=${text}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((data) => data.json());
+}
+
+export async function myAds(token) {
+  return fetch(`${catalogUrl}/myAds`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Authorization": token,
+    },
+  }).then((data) => data.json());
+}
+
+export async function getCakeById(cakeId) {
+  return fetch(`${catalogUrl}/${cakeId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((data) => data.json());
+}
+
+export async function createCake(cake, token) {
+  return fetch(`${catalogUrl}/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Authorization": token,
+    },
+    body: JSON.stringify(cake),
+  }).then((data) => data.json());
+}
+
+export async function editCake(cakeId, cake, token) {
+  return fetch(`${catalogUrl}/${cakeId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Authorization": token,
+    },
+    body: JSON.stringify(cake),
+  }).then((data) => data.json());
+}
+
+export async function deleteCake(cakeId, token) {
+  return fetch(`${catalogUrl}/${cakeId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Authorization": token,
+    },
+  });
 }
