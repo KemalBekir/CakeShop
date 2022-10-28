6d8bb999ea2a0035951a0570b29948e1f97a4176
KemalBekir <95451696+KemalBekir@users.noreply.github.com>
2022-10-28 17:43:49 +0100

    created basic catalogCard component

--- client/src/components/Catalog/Catalog.js
+++ client/src/components/Catalog/Catalog.js
@@ -1,16 +1,39 @@
 import { useCallback, useEffect, useState } from "react";
 import * as catalogService from "../../services/catalogServices";
+import CatalogCard from "../CatalogCard/CatalogCard";
 import "./Catalog.css";
 
 const Catalog = () => {
   const [cake, setCake] = useState([]);
 
   const data = useCallback(() => {
-    setCake(catalogService.getAll);
+    setCake(catalogService.getAll().then((result) => {
+        setCake(result);
+    }));
   }, []);
+
   useEffect(() => {
     data();
   }, [data]);
+
+  return (
+    <section className="catalog-section">
+      <div className="catalog-container">
+        {/* {isLoading ? (
+          <Spinner />
+        ) : (
+          <> */}
+            <h2 className="catalog-title">All listings</h2>
+            {cake.length > 0 ? (
+              cake.map((x) => <CatalogCard key={x._id} cake={x} />)
+            ) : (
+              <h3 style={{ color: "white" }}>No listings currently</h3>
+            )}
+          {/* </>
+        )} */}
+      </div>
+    </section>
+  );
 };
 
 export default Catalog;
---
+++ client/src/components/CatalogCard/CatalogCard.js
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import Moment from "react-moment";
+import "./CatalogCard.css";
+
+const CatalogCard = ({ cake }) => {
+  return (
+    <>
+      <div className="catalog-card-container">
+        <div className="catalog-img-container">
+          <img className="catalog-card-img" src={cake.img} alt="" />
+        </div>
+        <div className="catalog-card">
+          <h5 className="catalog-card-title">
+            {cake.cakeName.length > 16
+              ? `${cake.cakeName.substring(0, 16)}`
+              : cake.cakeName}
+          </h5>
+          {/* <p className="catalog-card-desc">
+            {cake.desc.length > 15
+              ? cake.desc.substring(0, 10)
+              : cake.desc}
+          </p> */}
+          {/* <p className="catalog-card-date">
+            <Moment format="MMMM Do YY, h:mm a">{cake.updatedAt}</Moment>
+          </p> */}
+        </div>
+        <Link className="catalog-card-link" to={`/details/${cake._id}`}>
+          Details
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default CatalogCard;
