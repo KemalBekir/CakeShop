2dd1f6b8c65c398755004a9f6ef1ee9f50c572b5
KemalBekir <95451696+KemalBekir@users.noreply.github.com>
2022-11-03 10:35:21 +0000

    styled  onOffer and imageCard

--- client/src/components/ImageCard/ImageCard.js
+++ client/src/components/ImageCard/ImageCard.js
@@ -1,15 +1,18 @@
+import { Link } from "react-router-dom";
 import "./ImageCard.css";
 
 const ImageCard = ({ cake }) => {
   return (
-    <div className="image-card">
-      <div className="image-card-container">
-        <img className="image" src={cake.imgOne} alt={cake.desc} />
+    <Link to={`/catalogue/details/${cake._id}`}>
+      <div className="image-card">
+        <div className="image-card-container">
+          <img className="image" src={cake.imgOne} alt={cake.desc} />
+        </div>
+        <div className="image-content-container">
+          <h3 className="image-title">Price: £{cake.price}</h3>
+        </div>
       </div>
-      <div className="image-content-container">
-        <h3 className="image-title">Price: £{cake.price}</h3>
-      </div>
-    </div>
+    </Link>
   );
 };
 
