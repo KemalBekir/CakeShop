7a0a55c15bfab9a3025d01ab312df9e03b177f3c
Kemal <outsider.pld@gmail.com>
2022-11-15 11:36:46 +0000

    added side bar to catalog and filter for type

--- client/src/components/Catalog/Catalog.js
+++ client/src/components/Catalog/Catalog.js
@@ -6,11 +6,25 @@ import React from "react";
 
 const Catalog = () => {
   const [cake, setCake] = useState([]);
+  let categories = [];
+
+  if (cake.length > 0) {
+    const result = cake.map((item) => item.type);
+    categories = [
+      "All",
+      ...result.filter((item, i) => {
+        return result.indexOf(item) === i;
+      }),
+    ];
+    console.log(categories);
+  }
 
   const data = useCallback(() => {
-    setCake(catalogService.getAll().then((result) => {
+    setCake(
+      catalogService.getAll().then((result) => {
         setCake(result);
-    }));
+      })
+    );
   }, []);
 
   useEffect(() => {
@@ -24,13 +38,21 @@ const Catalog = () => {
           <Spinner />
         ) : (
           <> */}
-            <h2 className="catalog-title">All listings</h2>
-            {cake.length > 0 ? (
-              cake.map((x) => <CatalogCard key={x._id} cake={x} />)
-            ) : (
-              <h3 style={{ color: "white" }}>No listings currently</h3>
-            )}
-          {/* </>
+        <h2 className="catalog-title">All listings</h2>
+        <div className="catalog-left-side">
+          <h3 className="catalog-categories">Categories:</h3>
+          <ul className="catalog-category-list">
+            {categories.map((x, i) => <li key={i}>{x}</li>)}
+          </ul>
+        </div>
+        <div className="catalog-content">
+          {cake.length > 0 ? (
+            cake.map((x) => <CatalogCard key={x._id} cake={x} />)
+          ) : (
+            <h3 style={{ color: "white" }}>No listings currently</h3>
+          )}
+        </div>
+        {/* </>
         )} */}
       </div>
     </section>
