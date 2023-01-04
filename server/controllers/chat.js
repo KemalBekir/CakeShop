bfed62db22afc4de048d8da22f796f23a2740f34
Kemal <outsider.pld@gmail.com>
2023-01-04 21:25:33 +0000

    edited chat controller

--- server/controllers/chat.js
+++ server/controllers/chat.js
@@ -5,47 +5,7 @@ const User = require("../models/user");
 const router = require("express").Router();
 
 router.post("/", isAuth(), async (req, res) => {
-  const { userId } = req.body;
-
-  if (!userId) {
-    res.status(400).json({ message: "UserId params not send with request" });
-  }
-
-  let isChat = await Chat.find({
-    isGroupChat: false,
-    $and: [
-      { users: { $elemMatch: { $eq: req.user._id } } },
-      { users: { $elemMatch: { $eq: userId } } },
-    ],
-  })
-    .populate("users", "-password")
-    .populate("latestMessage");
-
-  isChat = await User.populate(isChat, {
-    path: "latestMessage.sender",
-    select: "username",
-  });
-
-  let chatData;
-
-  if (isChat.length > 0) {
-    res.send(isChat[0]);
-  } else {
-    chatData = {
-      chatName: "sender",
-      isGroupChat: false,
-      users: [req.user._id, userId],
-    };
-  }
-
-  try {
-    const createdChat = await Chat.create(chatData);
-    const result = await Chat.findOne({ _id: createdChat._id }).populate(
-      "users",
-      "-password"
-    );
-    res.status(200).json(result);
-  } catch (err) {}
+ 
 });
 
 router.get("/", isAuth(), async (req, res) => {});
