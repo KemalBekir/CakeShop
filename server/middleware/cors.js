4fe234e10b4a508400dc084fda97bc58808355ab
Kemal <outsider.pld@gmail.com>
2023-04-18 23:40:06 +0100

    updated origin

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -1,5 +1,5 @@
 module.exports = () => (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://cakeshop.render.com');
+    res.setHeader('Access-Control-Allow-Origin', 'https://cakeshop-ej4v.onrender.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, HEAD,PUT, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Authorization');
     next();
